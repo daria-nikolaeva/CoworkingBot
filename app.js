e31147bf23a1209ae2e228a94ac79628e4b350aa
Daria <d.nikolaeva@geointellect.com>
2023-07-07 19:32:46 +0300

    small change

--- app.js
+++ app.js
@@ -6,12 +6,12 @@ tg.MainButton.color='#2cab37';
 
 let item ="";
 
-let btn1 = Element.getElementById("btn1");
-let btn2 = Element.getElementById("btn2");
-let btn3 = Element.getElementById("btn3");
-let btn4 = Element.getElementById("btn4");
-let btn5 = Element.getElementById("btn5");
-let btn6 = Element.getElementById("btn6");
+let btn1 = document.getElementById("btn1");
+let btn2 = document.getElementById("btn2");
+let btn3 = document.getElementById("btn3");
+let btn4 = document.getElementById("btn4");
+let btn5 = document.getElementById("btn5");
+let btn6 = document.getElementById("btn6");
 
 btn1.addEventListener("click",buttonClick("SMAS"));
 btn2.addEventListener("click",buttonClick("DOT"));
