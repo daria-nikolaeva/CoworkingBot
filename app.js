a1befc1188c8842cf7767b763e2fe1a46ed26aa0
Daria <d.nikolaeva@geointellect.com>
2023-07-13 22:20:53 +0300

    sml cng

--- app.js
+++ app.js
@@ -15,12 +15,12 @@ let btn4 = document.getElementById("btn4");
 let btn5 = document.getElementById("btn5");
 let btn6 = document.getElementById("btn6");
 
-btn1.addEventListener("click", finction(){ buttonClick("SMAS") });
-btn2.addEventListener("click", finction(){ buttonClick("DOT") });
-btn3.addEventListener("click", finction(){ buttonClick("SMAS") });
-btn4.addEventListener("click", finction(){ buttonClick("SMAS") });
-btn5.addEventListener("click", finction(){ buttonClick("SMAS") });
-btn6.addEventListener("click", finction(){ buttonClick("SMAS") });
+btn1.addEventListener("click", function() { buttonClick("SMAS"); });
+btn2.addEventListener("click", function () {buttonClick("DOT"); });
+btn3.addEventListener("click", function () {buttonClick("SMAS"); });
+btn4.addEventListener("click", function () {buttonClick("SMAS"); });
+btn5.addEventListener("click", function () {buttonClick("SMAS"); });
+btn6.addEventListener("click", function () {buttonClick("SMAS"); });
 
 
 function buttonClick(itemName){
